164f03231eb71ff00ffeec9c1668648681ccd850
Vitor Duarte Chaves <vitord.chaves@gmail.com>
2025-06-07 09:56:32 -0300

    Função analisar humor

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
 function analisarHumor(texto) {
-    const positivo = ['feliz','alegre','animado','grato']
-    const negativo = ['triste','cansado','chateado','']
+    const positivo = ['feliz','alegre','animado','grato','ótimo','bem']
+    const negativo = ['triste','cansado','chateado','pessimo']
+    const textoMin = texto.lowerCase();
+    let score = 0;
+    positivo.forEach(palavra => {
+        if (textoMin.includes(palavra) ) score++
+    })
+    negativo.forEach(palavra => {
+        if (textoMin.includes(palavra) ) score-- 
+    })
+    if (score > 0) return {humor: 'Feliz', emoji: '😄'};
+    else if (score < 0) return {humor: 'Triste', emoji: '😭'};
+    else return {humor: 'Neutro', emoji: '😐'};
+}
+
+function salvarEntrada() {
+    const usuario = document.getElementById ("usuario").value.trim();
+    const data = document.getElementById ("data").value;
+    const titulo = document.getElementById ("titulo").value.trim();
+    const texto = document.getElementById ("texto").value.trim();
+    if (!usuario || !data || !titulo || !texto) {
+        alert ("Por favor, preencha todos os campos.");
+        return;
+    }
+    const analise = analisarHumor(texto);
+    const novaEntrada = {
+        usuario,
+        data,
+        titulo,
+        texto,
+        humor: analise.humor,
+        emoji: analise.emoji
+    };
 }
